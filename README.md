d53d95f474fb183765155e77957745b86439f02a
Curtis Kelsey <curtis.kelsey@gmail.com>
2015-08-14 07:23:34 -0500

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+# May the Force be Menu
+
+## Requirements
+* [d3.js](http://d3js.org/)
+
+## Introduction
+I got tired of the same menus all across the internet. Navigation is a base necessity for every site. Let's dress it
+up and make it into something interactive.
---
+++ force-be-menu.js
@@ -0,0 +1,355 @@
+/**
+ * Initialize with a d3 selector of the navigation's ul or a JSON array of navigation objects
+ * @constructor
+ */
+var ForceMenu = function(navigationData, options) {
+
+    var _log = {},
+        _dataSet = navigationData,
+        width = window.innerWidth,
+        height = window.innerHeight,
+        diagonal = Math.sqrt(width * width + height * height),
+        force = null,
+        root = null,
+        svg = null,
+        overlay = null,
+        links = null,
+        nodes = null,
+        circles = null,
+        text = null,
+        title = document.title,
+        maxNameLength = null,
+        ref = this,
+        _options = {
+            'gravity': '0.05',
+            'charge': '-30',
+            'navigationFillColor': '#AABED8',
+            'navigationStrokeColor': '#999999',
+            'navigationRadius': diagonal/20,
+            'rootFillColor': 'rgb(31, 119, 180)',
+            'rootStrokeColor': '#999999',
+            'rootRadius': diagonal/20,
+            'lineStrokeColor': '#666666',
+            'lineStrokeWidth': '1px',
+            'lineDistance': diagonal/5,
+            'textStrokeColor': '#000000',
+            'textSize': '',
+            'container': 'body'
+        };
+
+    // Create an object with functions that mimic the window.console object made
+    // available by tools like Firebug or the "Dev Tools" add-on in IE8+
+    _log.dummyConsole = {
+        assert : function(){},
+        log : function(){},
+        warn : function(){},
+        error : function(){},
+        debug : function(){},
+        dir : function(){},
+        info : function(){}
+    };
+
+    // Throughout our app we'll make console/logging calls by using the myapp.console
+    // object. Example: myapp.console.debug("blerg!"). By default, the myapp.console
+    // object should use the "dummy" console that doesn't do anything.
+    _log.console = _log.dummyConsole;
+
+    // This function can be used to switch the myapp.console variable to use the
+    // real window.console object. Note that it does a safety check to ensure that
+    // window.console actually exists (it won't in browsers not running Firebug or
+    // the IE dev tools).
+    _log.enableConsoleOutput = function(enable) {
+        // Don't enable the console unless it actually exists
+        if (enable && window.console !== undefined) {
+            _log.console = window.console;
+        } else {
+            _log.console = _log.dummyConsole;
+        }
+    };
+
+    /**
+     * Initialize the force menu
+     */
+    function initialize() {
+
+        _log.enableConsoleOutput(true);
+
+        _log.console.debug("Initializing...");
+
+        if (typeof d3 == 'undefined') {
+            throw "d3.js is not loaded.";
+        }
+
+        switch(detectDataType()){
+            case 'element':
+                convertElementToDataSet();
+                break;
+            default:
+                break;
+        }
+
+        maxNameLength = d3.max(_dataSet.nodes, function(d){ return d.name.length; });
+        _options.textSize = diagonal/(10 * (maxNameLength/1.5));
+
+        constructLinks();
+
+        setupRootNode();
+
+        setupDisplay();
+
+        run();
+    }
+
+    /**
+     * Determine if the argument provided is a selector or an array of JSON objects
+     */
+    function detectDataType() {
+
+        if (typeof navigationData[0] == 'undefined') {
+            return 'dataset';
+        }
+
+        if (typeof navigationData[0][0] == 'undefined') {
+            return 'dataset';
+        }
+
+        if (navigationData[0][0] instanceof Element) {
+
+            _log.console.debug('The constructor data is a selector.');
+            return 'element';
+        }
+
+        _log.console.debug('The constructor data is a data set.');
+        return 'dataset';
+    }
+
+    function convertElementToDataSet() {
+
+        _log.console.debug("Converting element selector to data set...");
+
+        var navigationItems = _dataSet.getElementsByTagName("a");
+        _log.console.debug(navigationItems);
+
+        var tempDataSet = [];
+
+        navigationItems.each(function(d, i){
+
+            _log.console.debug(d);
+            _log.console.debug(i);
+            _log.console.debug(navigationItems[i]);
+
+            //tempDataSet.push({"name":d.text(),"group":1,"url":d.attr("href")});
+        });
+
+        _dataSet = tempDataSet;
+    }
+
+    /**
+     * Setup a root node for the navigation to attach to
+     */
+    function setupRootNode() {
+
+        var rootNode = {"name":title,"group":2,"radius":10,"url":"#"};
+
+        _dataSet.nodes.unshift(rootNode);
+
+        root = _dataSet.nodes[0];
+        root.x = width/2;
+        root.y = height/2;
+        root.radius = 10;
+        root.fixed = true;
+
+        _log.console.debug(_dataSet);
+    }
+
+    /**
+     * Define all of the links between the navigation nodes and the root node
+     */
+    function constructLinks() {
+
+        _dataSet.links = [];
+
+        for (var i = 1; i <= _dataSet.nodes.length; i++) {
+
+            _dataSet.links.push({"source":i,"target":0,"value":10});
+        }
+    }
+
+    function setupDisplay() {
+
+        overlay = d3.select(_options.container)
+            .append("div")
+            .attr("class", "svg-container")
+            .style("display", "none");
+
+        // Setup the display
+        svg = d3.select(_options.container)
+            .append("svg:svg")
+            .attr("width", width)
+            .attr("height", height)
+            .style("display", "none");
+
+        links = svg.selectAll("line")
+            .data(_dataSet.links)
+            .enter().append("svg:line")
+            .attr("class", "link")
+            .style("stroke-width", _options.lineStrokeWidth)
+            .style("stroke", _options.lineStrokeColor);
+
+        nodes = svg.selectAll("circle")
+            .data(_dataSet.nodes)
+            .enter()
+            .append("svg:g");
+
+        circles = nodes.append("svg:circle")
+            .attr("class", "node")
+            .attr("r", function (d){
+                if (d.name == title) {
+                    return _options.rootRadius;
+                }
+
+                return _options.navigationRadius;
+            })
+            .style("fill", function(d) {
+                if (d.name == title) {
+                    return _options.rootFillColor;
+                }
+
+                return _options.navigationFillColor;
+            })
+            .style("stroke", function(d){
+                if (d.name == title) {
+                    return _options.rootStrokeColor;
+                }
+
+                return _options.navigationStrokeColor;
+            });
+
+        text = nodes.append("svg:text")
+            .attr("class", "text")
+            .attr("text-anchor", "middle")
+            .attr("alignment-baseline", "central")
+            .attr("stroke", _options.textStrokeColor)
+            .attr("font-size", _options.textSize)
+            .text(function(d) {
+                return d.name;
+            });
+    }
+
+    /**
+     * Run the display
+     */
+    function run() {
+
+        force = d3.layout.force()
+            .gravity(_options.gravity)
+            .charge(_options.charge)
+            .nodes(_dataSet.nodes)
+            .links(_dataSet.links)
+            .linkDistance(_options.lineDistance)
+            .size([width, height]);
+
+        // On each tick of the display
+        force.on("tick", function (e) {
+            var q = d3.geom.quadtree(_dataSet.nodes),
+                    i = 0,
+                    n = _dataSet.nodes.length;
+
+            while (++i < n) {
+                q.visit(collide(_dataSet.nodes[i]));
+            }
+
+            nodes.attr("transform",  function(d) {
+                return "translate(" + d.x + "," + d.y + ")";
+            });
+
+            links.attr("x1", function(d) { return d.source.x; })
+                .attr("y1", function(d) { return d.source.y; })
+                .attr("x2", function(d) { return d.target.x; })
+                .attr("y2", function(d) { return d.target.y; });
+        });
+
+        // When a menu item is clicked, navigate to that location
+        nodes.on("click", function(e){
+
+            _log.console.debug(e);
+
+            if (e.url == '#') {
+                ref.hide();
+            }
+
+            location.assign(e.url);
+        });
+
+        //svg.on("click", function(e){
+        //   ref.hide();
+        //});
+
+        circles.call(force.drag);
+    }
+
+    this.hide = function() {
+
+        if (overlay != null) {
+            overlay.style("display","none");
+        }
+
+        if (svg != null) {
+            svg.style("display", "none");
+        }
+
+        force.stop();
+    }
+
+    this.show = function() {
+
+        if (overlay != null) {
+            overlay.style("display","inline");
+        }
+
+        if (svg != null) {
+            svg.style("display", "inline");
+        }
+
+        force.start();
+    }
+
+    /**
+     * Handle menu item collisions
+     */
+    function collide(node) {
+
+        var r = node.radius + 16,
+                nx1 = node.x - r,
+                nx2 = node.x + r,
+                ny1 = node.y - r,
+                ny2 = node.y + r;
+
+        return function (quad, x1, y1, x2, y2) {
+
+            if (quad.point && (quad.point !== node)) {
+
+                var x = node.x - quad.point.x,
+                    y = node.y - quad.point.y,
+                    l = Math.sqrt(x * x + y * y),
+                    r = node.radius + quad.point.radius;
+
+                if (l < r) {
+
+                    l = (l - r) / l * .5;
+                    node.x -= x *= l;
+                    node.y -= y *= l;
+                    quad.point.x += x;
+                    quad.point.y += y;
+                }
+            }
+
+            return x1 > nx2
+                || x2 < nx1
+                || y1 > ny2
+                || y2 < ny1;
+        };
+    }
+
+    initialize();
+}
